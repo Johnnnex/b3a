fd013bb2d585fbc462a23b37d2fd8cb2782b3c0c
Johnnnex <onyeiborjohn05@gmail.com>
2023-08-04 17:31:54 +0100

    description

--- blockfest-africa/layout/HeadComp.jsx
+++ blockfest-africa/layout/HeadComp.jsx
@@ -5,7 +5,7 @@ const HeadComp = ({title}) => {
   return (
     <Head>
         <title>{title}</title>
-        <meta name="description" content="Alchemy, Challenging the boundaries of possibilities with shameless brand positioning and skill presentation that drlivers extraordinary results" />
+        <meta name="description" content="BLOCKF3ST is an annual developers, creatives, digital creators, traders festival that caters to the Web3 community." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css"/>
         <link rel="icon" href="/logo.ico" />
