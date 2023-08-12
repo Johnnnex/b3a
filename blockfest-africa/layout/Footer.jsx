efd432db2b88a833fceec8e715efe9ca7541b098
Johnnnex <onyeiborjohn05@gmail.com>
2023-08-12 03:46:50 +0100

    blockfest africa

--- blockfest-africa/layout/Footer.jsx
+++ blockfest-africa/layout/Footer.jsx
@@ -62,40 +62,21 @@ const Footer = () => {
                     <Image className="cursor-pointer" src="/Images/logo.svg" alt="logo" height={50} width={100} />
                 </Link>
             </div>
-          <div className="flex md:w-[45%] items-center w-[50%] justify-between">
-            <a href="https://instagram.com/blockfestafrica?igshid=MzRlODBiNWFlZA==">
-              <Image src="/Images/ig.svg" alt="logo" height={20} width={20} />
+        </div>
+        <div className="flex items-center md:w-[20%] w-[70%] mx-auto justify-between">
+            <a className="flex justify-between neumorphic items-center p-3 bg-[#00000090] hover:bg-black transition-[.5s] rounded-[50%]" href="https://instagram.com/blockfestafrica?igshid=MzRlODBiNWFlZA==">
+              <Image src="/Images/instagram.svg" alt="logo" height={20} width={20} />
             </a>
-            <a href="https://twitter.com/blockfestafrica?t=HrZZPme5kS-ml1nplc6NeQ&s=08" target="_blank" rel="noopener noreferrer">
-              <Image src="/Images/twitter.svg" alt="logo" height={20} width={20} />
+            <a className="flex justify-between items-center p-3 bg-[#00000090] hover:bg-black transition-[.5s] rounded-[50%]" href="https://twitter.com/blockfestafrica?t=HrZZPme5kS-ml1nplc6NeQ&s=08" target="_blank" rel="noopener noreferrer">
+              <Image src="/Images/twt.svg" alt="logo" height={20} width={20} />
             </a>
-            <a href="https://www.linkedin.com/company/blockfest-africa/" target="_blank" rel="noopener noreferrer">
-              <Image src="/Images/linkedin.svg" alt="logo" height={20} width={20} />
+            <a className="flex justify-between items-center p-3 bg-[#00000090] hover:bg-black transition-[.5s] rounded-[50%]" href="https://www.linkedin.com/company/blockfest-africa/" target="_blank" rel="noopener noreferrer">
+              <Image src="/Images/in.svg" alt="logo" height={20} width={20} />
             </a>
-            <a href="https://www.youtube.com/@BLOCKFESTAFRICA" target="_blank" rel="noopener noreferrer">
-              <Image src="/Images/youtube.svg" alt="logo" height={20} width={25} />
+            <a className="flex justify-between items-center p-3 bg-[#00000090] hover:bg-black transition-[.5s] rounded-[50%]" href="https://www.youtube.com/@BLOCKFESTAFRICA" target="_blank" rel="noopener noreferrer">
+              <Image src="/Images/utube.svg" alt="logo" height={20} width={25} />
             </a>
           </div>
-        </div>
-        <nav className="w-[70%] pl-5 md:pl-0">
-          <ul className="flex flex-col md:flex-row justify-evenly font-poppins text-white">
-            <Link href="#about">
-              <li className={`${about} cursor-pointer hover:text-[#1A63E4] transition-[.5s]`}>About</li>
-            </Link>
-            <Link href="#speakers">
-              <li className={`${speaker} cursor-pointer hover:text-[#1A63E4] transition-[.5s]`}>Speakers</li>
-            </Link>
-            <Link href="#patners" className="cursor-pointer">
-              <li className={`${partner} cursor-pointer hover:text-[#1A63E4] transition-[.5s]`}>Partners</li>
-            </Link>
-            <Link href="#schedule" className="cursor-pointer">
-              <li className={`${schedule} cursor-pointer hover:text-[#1A63E4] transition-[.5s]`}>Event Schedule</li>
-            </Link>
-            <Link href="#FAQ" className="cursor-pointer">
-              <li className={`${faq} cursor-pointer hover:text-[#1A63E4] transition-[.5s]`}>FAQ</li>
-            </Link>
-          </ul>
-        </nav>
         <div className="md:w-[40%]"></div>
       </div>
     </footer>
